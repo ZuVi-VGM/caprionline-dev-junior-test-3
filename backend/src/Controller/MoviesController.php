c037be5418d7798e83212cf28203cedbdfdb5a36
Vito Gabriele Marino <vitog.m98@gmail.com>
2024-05-07 18:21:07 +0200

    Switched to entity manager

--- backend/src/Controller/MoviesController.php
+++ backend/src/Controller/MoviesController.php
@@ -3,43 +3,44 @@
 namespace App\Controller;
 
 use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
-use App\Repository\MovieGenreRepository;
+use App\Entity\Movie;
+use App\Entity\MovieGenre;
 
 #[Route('/movies', name: 'movies_')]
 class MoviesController extends AbstractController
 {
     public function __construct(
-        private MovieRepository $movieRepository,
-        //Temporary
-        private MovieGenreRepository $movieGenreRepository,
+        private EntityManagerInterface $entityManager,
         private SerializerInterface $serializer
     ) {}
 
-    // Get film list
+    // Get movie list
     #[Route('', methods: ['GET'], name: 'list')]
     public function list(): JsonResponse
     {
-        $movies = $this->movieRepository->findAll();
+        $movies = $this->entityManager->getRepository(Movie::class)->findAll();
         $data = $this->serializer->serialize($movies, "json", ["groups" => "default"]);
 
         return new JsonResponse($data, json: true);
     }
 
-    // Sort films
+    // Sort movies
     #[Route('/{orderBy}', methods: ['GET'], requirements: ['orderBy'=>'^(?=.*[a-zA-Z])[a-zA-Z0-9]+$'], name: 'sort')]
     public function sort(string $orderBy): JsonResponse
     {
+        $movieRepository = $this->entityManager->getRepository(Movie::class);
         $movies = [];
 
         if($orderBy == 'release')
-            $movies = $this->movieRepository->findBy([], ['releaseDate' => 'DESC']);
+            $movies = $movieRepository->findBy([], ['releaseDate' => 'DESC']);
         elseif ($orderBy == 'rating')
-            $movies = $this->movieRepository->findBy([], ['rating' => 'DESC']);
+            $movies = $movieRepository->findBy([], ['rating' => 'DESC']);
 
         $data = $this->serializer->serialize($movies, "json", ["groups" => "default"]);
 
@@ -50,9 +51,10 @@ class MoviesController extends AbstractController
     #[Route('/{genre}', methods: ['GET'], requirements: ['genre' => '\d+'], name: 'genre_list')]
     public function listByGenre(int $genre): JsonResponse
     {
+        $movieGenreRepository = $this->entityManager->getRepository(MovieGenre::class);
         $movies = [];
 
-        $movieForGenre = $this->movieGenreRepository->findByGenreIdJoinedToMovie($genre);
+        $movieForGenre = $movieGenreRepository->findByGenreIdJoinedToMovie($genre);
         
         foreach($movieForGenre as $movieData)
             $movies[] = $movieData->getMovie();
@@ -66,14 +68,14 @@ class MoviesController extends AbstractController
     #[Route('/{genre}/{orderBy}', methods: ['GET'], name: 'genre_sort')]
     public function sortByGenre(int $genre, string $orderBy): JsonResponse
     {
-        //TODO: maybe a better error handling
+        $movieGenreRepository = $this->entityManager->getRepository(MovieGenre::class);
         $movies = [];
         $movieForGenre = [];
 
         if($orderBy == 'release')
-            $movieForGenre = $this->movieGenreRepository->findByGenreIdJoinedToMovie($genre, orderBy: ['c.releaseDate' => 'DESC']);
+            $movieForGenre = $movieGenreRepository->findByGenreIdJoinedToMovie($genre, orderBy: ['c.releaseDate' => 'DESC']);
         elseif ($orderBy == 'rating')
-            $movieForGenre = $this->movieGenreRepository->findByGenreIdJoinedToMovie($genre, orderBy: ['c.rating' => 'DESC']);
+            $movieForGenre = $movieGenreRepository->findByGenreIdJoinedToMovie($genre, orderBy: ['c.rating' => 'DESC']);
 
         foreach($movieForGenre as $movieData)
             $movies[] = $movieData->getMovie();
